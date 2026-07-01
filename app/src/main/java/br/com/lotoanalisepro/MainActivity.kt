6e6f31ed79eb9c5044cab2db33a5d06284100c02
SkafeBinho <skafesc@gmail.com>
2026-07-01 14:13:33 -0300

    Add MainActivity with LotoApp composable

---
+++ app/src/main/java/br/com/lotoanalisepro/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.com.lotoanalisepro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            LotoApp()
+        }
+    }
+}
+
+@Composable
+fun LotoApp() {
+    MaterialTheme {
+        Surface {
+            Text(text = "Loto Análise Pro 🚀")
+        }
+    }
+}
