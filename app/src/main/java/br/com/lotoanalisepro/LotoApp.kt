c733f3853d844c86527ae6bbb5eee8f538e3f579
SkafeBinho <skafesc@gmail.com>
2026-07-01 14:30:39 -0300

    Wrap LotoApp in LotoTheme for theming support

--- app/src/main/java/br/com/lotoanalisepro/LotoApp.kt
+++ app/src/main/java/br/com/lotoanalisepro/LotoApp.kt
@@ -3,10 +3,12 @@ package br.com.lotoanalisepro
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import br.com.lotoanalisepro.navigation.AppNavigation
+import br.com.lotoanalisepro.ui.theme.LotoTheme
 
 @Composable
 fun LotoApp() {
-    MaterialTheme {
+    LotoTheme {
         Surface {
             AppNavigation()
         }
